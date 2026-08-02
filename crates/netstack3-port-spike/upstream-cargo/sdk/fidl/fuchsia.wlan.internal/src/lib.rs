403a69dda3079d5663d2711538ba97a8355b999c
maan2003 <manmeetmann2003@gmail.com>
2026-08-02 13:38:37 +0000

    package pinned Fuchsia WLAN security bindings

---
+++ crates/netstack3-port-spike/upstream-cargo/sdk/fidl/fuchsia.wlan.internal/src/lib.rs
@@ -0,0 +1,125 @@
+// Copyright 2026 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Host subset generated from the pinned `fuchsia.wlan.internal` schema.
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct WepCredentials {
+    pub key: Vec<u8>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum WpaCredentials {
+    Psk([u8; 32]),
+    Passphrase(Vec<u8>),
+    #[doc(hidden)]
+    __Unknown {
+        ordinal: u64,
+        bytes: Vec<u8>,
+    },
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Credentials {
+    Wep(WepCredentials),
+    Wpa(WpaCredentials),
+    #[doc(hidden)]
+    __Unknown {
+        ordinal: u64,
+        bytes: Vec<u8>,
+    },
+}
+
+impl Credentials {
+    #[allow(clippy::boxed_local)]
+    pub fn into_wep(self: Box<Self>) -> Option<WepCredentials> {
+        match *self {
+            Self::Wep(credentials) => Some(credentials),
+            _ => None,
+        }
+    }
+
+    #[allow(clippy::boxed_local)]
+    pub fn into_wpa(self: Box<Self>) -> Option<WpaCredentials> {
+        match *self {
+            Self::Wpa(credentials) => Some(credentials),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[repr(transparent)]
+pub struct Protocol(u32);
+
+#[allow(non_upper_case_globals)]
+impl Protocol {
+    pub const Open: Self = Self(1);
+    pub const Wep: Self = Self(2);
+    pub const Wpa1: Self = Self(3);
+    pub const Wpa2Personal: Self = Self(4);
+    pub const Wpa2Enterprise: Self = Self(5);
+    pub const Wpa3Personal: Self = Self(6);
+    pub const Wpa3Enterprise: Self = Self(7);
+    pub const Owe: Self = Self(8);
+
+    pub const fn from_primitive(value: u32) -> Self {
+        Self(value)
+    }
+
+    pub const fn into_primitive(self) -> u32 {
+        self.0
+    }
+
+    pub const fn unknown() -> Self {
+        Self(u32::MAX)
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Authentication {
+    pub protocol: Protocol,
+    pub credentials: Option<Box<Credentials>>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn schema_values_match_pinned_fidl() {
+        assert_eq!(Protocol::Open.into_primitive(), 1);
+        assert_eq!(Protocol::Wpa3Enterprise.into_primitive(), 7);
+        assert_eq!(Protocol::Owe.into_primitive(), 8);
+        assert_eq!(Protocol::from_primitive(44).into_primitive(), 44);
+    }
+
+    #[test]
+    fn credentials_preserve_schema_shapes() {
+        let psk = WpaCredentials::Psk([7; 32]);
+        let credentials = Box::new(Credentials::Wpa(psk.clone()));
+        assert_eq!(credentials.into_wpa(), Some(psk));
+
+        let passphrase = WpaCredentials::Passphrase(vec![b'x'; 63]);
+        assert_eq!(
+            Credentials::Wpa(passphrase.clone()),
+            Credentials::Wpa(passphrase)
+        );
+    }
+
+    #[test]
+    fn flexible_unions_retain_opaque_unknown_payloads() {
+        let credentials = Credentials::__Unknown {
+            ordinal: 9,
+            bytes: vec![1, 2, 3],
+        };
+        assert_eq!(
+            credentials,
+            Credentials::__Unknown {
+                ordinal: 9,
+                bytes: vec![1, 2, 3]
+            }
+        );
+    }
+}
