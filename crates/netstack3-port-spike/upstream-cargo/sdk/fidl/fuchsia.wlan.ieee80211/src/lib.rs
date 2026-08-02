9172d5883e9eca190f74aa46fae73c37fcf9b274
maan2003 <manmeetmann2003@gmail.com>
2026-08-02 14:07:54 +0000

    package pinned Fuchsia SAE and OWE crypto

--- crates/netstack3-port-spike/upstream-cargo/sdk/fidl/fuchsia.wlan.ieee80211/src/lib.rs
+++ crates/netstack3-port-spike/upstream-cargo/sdk/fidl/fuchsia.wlan.ieee80211/src/lib.rs
@@ -108,7 +108,10 @@ flexible_code!(StatusCode, valid_status_code);
 
 #[allow(non_upper_case_globals)]
 impl StatusCode {
+    pub const Success: Self = Self(0);
     pub const RefusedReasonUnspecified: Self = Self(1);
+    pub const AntiCloggingTokenRequired: Self = Self(76);
+    pub const SaeHashToElement: Self = Self(126);
 }
 
 flexible_enum!(ChannelBandwidth, u32, ChannelBandwidthUnknown, {
@@ -198,6 +201,9 @@ mod tests {
         assert_eq!(WlanBand::FiveGhz.into_primitive(), 1);
         assert_eq!(BssType::Personal.into_primitive(), 4);
         assert_eq!(WlanPhyType::He.into_primitive(), 12);
+        assert_eq!(StatusCode::Success.into_primitive(), 0);
+        assert_eq!(StatusCode::AntiCloggingTokenRequired.into_primitive(), 76);
+        assert_eq!(StatusCode::SaeHashToElement.into_primitive(), 126);
         assert_eq!(std::mem::size_of::<MacAddr>(), MAC_ADDR_LEN as usize);
         assert_eq!(
             HtCapabilities { bytes: [0; 26] }.bytes.len(),
