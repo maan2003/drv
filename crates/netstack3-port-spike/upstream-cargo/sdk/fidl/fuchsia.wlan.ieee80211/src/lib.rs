9f757872e291534bc78625b2d5c93f901fea5701
maan2003 <manmeetmann2003@gmail.com>
2026-08-02 13:18:29 +0000

    package pinned Fuchsia IEEE 802.11 types

---
+++ crates/netstack3-port-spike/upstream-cargo/sdk/fidl/fuchsia.wlan.ieee80211/src/lib.rs
@@ -0,0 +1,8 @@
+// Copyright 2021 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//! Host subset generated from the pinned `fuchsia.wlan.ieee80211` schema.
+
+pub const MAX_SSID_BYTE_LEN: u8 = 32;
+pub const MAC_ADDR_LEN: u8 = 6;
